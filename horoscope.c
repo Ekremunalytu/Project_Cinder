dd16a0150e711d01f35349014976607633c7522e
Ekremunalytu <102416114+Ekremunalytu@users.noreply.github.com>
2022-04-14 18:05:16 +0300

    Add files via upload

---
+++ horoscope.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int Birth_Time(int day,int month,int year);
+
+
+int main() {
+	int day;
+	int month;
+	int year;
+
+	printf("Please enter the day you born \n>>");
+	scanf("%d",&day);
+
+	if (day > 31){
+		printf("Please enter a valid number !!!");
+		return 0;
+	}
+
+	printf("Please enter the month you born\n>> ");
+	scanf("%d",&month);
+
+	if (month > 12 || month == 0){
+		printf("Please enter a valid number");
+		return 0;
+	}
+
+	printf("Please enter the year you born \n>>");
+	scanf("%d",&year);
+	
+	if (year>2022 || year< 1900){
+		printf("Please enter a valid number!!!\n");
+		return 0;
+	}
+	return 0;
+	
+}
