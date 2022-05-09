afc19ff3b16543bf3a5c51abba052850990f5d44
Ekremunalytu <102416114+Ekremunalytu@users.noreply.github.com>
2022-05-09 18:21:37 +0300

    Update horoscope.c

--- horoscope.c
+++ horoscope.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 
-int Birth_Time(int day,int month,int year);
+int Birth_Time(int day, int month, int year);
 
 
 int main() {
@@ -13,41 +13,59 @@ int main() {
 	int year;
 
 	printf("Please enter the day you born \n>>");
-	scanf("%d",&day);
+	scanf_s("%d", &day);
 
-	if (day > 31){
+	if (day > 31) {
 		printf("Please enter a valid number !!!");
 		return 0;
 	}
 
 	printf("Please enter the month you born\n>> ");
-	scanf("%d",&month);
+	scanf_s("%d", &month);
 
-	if (month > 12 || month == 0){
+	if (month > 12 || month == 0) {
 		printf("Please enter a valid number");
 		return 0;
 	}
 
 	printf("Please enter the year you born \n>>");
-	scanf("%d",&year);
-	
-	if (year>2022 || year< 1900){
+	scanf_s("%d", &year);
+
+	if (year > 2022 || year < 1900) {
 		printf("Please enter a valid number!!!\n");
 		return 0;
-	}
-	
-	struct horoscopes{
-		skills,
-		storng_sides,
-		weak_sides,
-		potencial_future
-		};
-	struct horoscopes capricorn[1];
-	
-	struct *pointer_capricorn = &capricorn;
-	
-	strcpy(pointer_capricorn->skills,"example skills");
-		
+	};
+	struct Horoscopes {
+		char Name[50];
+		char Skills[50];
+		char Weaknesses[50];
+		char Strong_Sides[50];
+		char Potential_Future[50];
+	} Capricorn;
+	strcpy_s(Capricorn.Name, 10, "Capricorn");
+	strcpy_s(Capricorn.Skills,20, "Good");
+	strcpy_s(Capricorn.Weaknesses,20, "To naive");
+	strcpy_s(Capricorn.Strong_Sides,20, "Well hearted");
+	strcpy_s(Capricorn.Potential_Future,20, "Shiny");
+	switch (month) {
+		case 1: {
+			printf("Your horoscope is  %s \n", Capricorn.Name);
+			printf("Skil:  %s\n", Capricorn.Skills);
+			printf("Weakness: %s\n", Capricorn.Weaknesses);
+			printf("Strong_sides: %s\n", Capricorn.Strong_Sides);
+			printf("Potential future: %s\n", Capricorn.Potential_Future);
+		}
+		default:
+			printf("Please try again");
+	};
+
+
+
+
+
+
+
+
+
+
 	return 0;
-	
-}
